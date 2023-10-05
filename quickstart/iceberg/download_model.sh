eb898f789e5e559f41b5a4c032ead75bf005e3b6
sam goldman <samlg@molgpu02.mit.edu>
2023-10-05 12:59:51 -0400

    Add simple scarf demo

--- quickstart/iceberg/download_model.sh
+++ quickstart/iceberg/download_model.sh
@@ -1,7 +1,13 @@
 #wget ??nist_iceberg_models.zip
 
-wget https://www.dropbox.com/s/omark23l0ee22hf/canopus_iceberg_models.zip
-unzip canopus_iceberg_models
+# wget https://www.dropbox.com/s/omark23l0ee22hf/canopus_iceberg_models.zip
+
+# wget but send output to new file
+wget https://www.dropbox.com/scl/fi/1g3d4mx6yrh5rkj56hucs/canopus_iceberg_models.zip?rlkey=be8h3m02jy4ondn5tr8sbcfsh -O canopus_iceberg_models.zip  
+
+
+unzip canopus_iceberg_models.zip
+
 
 mkdir quickstart/iceberg/models
 
